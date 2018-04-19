c18abc848ecade2f5258e72355a310d4ea6a99b7
Pietro Bongiovanni <pietro@fixico.nl>
2018-04-19 13:37:19 +0200

    Added crashing route to show auto-restarting pods

--- src/main.go
+++ src/main.go
@@ -4,15 +4,27 @@ import (
 	"fmt"
     "log"
     "net/http"
+    "os"
 )
 
 func main() {
+
+    fmt.Println("Started Application, listening on port 8080")
+
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Println("Recorded Visit to /")
     })
 
-    err := http.ListenAndServe(":8000", nil)
+    http.HandleFunc("/crash", func(w http.ResponseWriter, r *http.Request) {
+        w.WriteHeader(http.StatusOK)
+
+        fmt.Println("Bye Bye");
+
+        os.Exit(1)
+    })
+
+    err := http.ListenAndServe(":8080", nil)
     log.Fatal(err)
 }
 
